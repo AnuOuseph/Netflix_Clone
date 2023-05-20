b4ee73bdbdd93aa9410f74e794590dd35584db88
AnuOuseph <anuouseph04@gmail.com>
2023-05-20 21:39:39 +0530

    video urls added

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -3,16 +3,37 @@ import './Banner.css'
 import axios from '../../axios'
 import {apiKey} from '../../constants/constant'
 import { imgUrl } from '../../constants/constant'
+import YouTube from 'react-youtube';
 
 function Banner() {       
-  const [movie,setMovie] = useState()                                         
+  const [movie,setMovie] = useState()
+  const [bannerUrl,setBannerUrl] = useState('')                                         
   useEffect(()=>{
     axios.get(`/trending/all/week?api_key=${apiKey}&language=en-US`).then((response)=>{
+      const randomIndex = Math.floor(Math.random() * response.data.results.length);
      //console.log(response.data.results[1]);
-      setMovie(response.data.results[1])
+      setMovie(response.data.results[randomIndex])
     })
   },[])
+  const loadVideo = (id)=>{
+    axios.get(`/movie/${id}/videos?api_key=${apiKey}&language=en-US`).then((response)=>{
+      console.log(response.data);
+      if(response.data.results.length !== 0){
+      setBannerUrl(response.data.results[0])
+      }else{
+        console.log('array empty');
+      }
+    })
+  }
+  const opt = {
+    height: '390',
+    width: '100%',                
+    playerVars: {
+      autoplay: 0,
+    },
+  };
   return (
+    <>
     <div style={{backgroundImage:`url(${movie?imgUrl+movie.backdrop_path:""})`}}
      className='banner'>
       <div className="content">
@@ -20,15 +41,19 @@ function Banner() {
             {movie?movie.title:""}
         </h1>
         <div className="buttons">
-            <button className='button'>Play</button>
+            <button className='button' onClick={()=>loadVideo(movie?movie.id:"")}>Play</button>
             <button className='button'>+ My List</button>
         </div>
         <h1 className="description">
           {movie?movie.overview:""}
         </h1>
       </div>
-      <div className="fade"></div>
+      <div className="fade"></div> 
     </div>
+    <div className='col-md-12' style={{margin:40}}>
+    {bannerUrl?<YouTube opts={opt} videoId={bannerUrl.key}/>:""}
+    </div>
+    </>
   )
 }
 
--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,27 +1,49 @@
-
+import YouTube from 'react-youtube';
 import React,{useEffect, useState} from 'react'
 import axios from '../../axios'
-//import { apiKey } from '../../constants/constant'
+import { apiKey } from '../../constants/constant'
 import { imgUrl } from '../../constants/constant'
 import './Card.css'
 
 function Card(props) {
   const [row,setRow] = useState([])
+  const [url,setUrl] = useState('')
   useEffect(()=>{
     axios.get(props.url).then((response)=>{
-      console.log(response.data)
+      //console.log(response.data)
       setRow(response.data.results)
     })
   })
+  const opts = {
+    height: '390',
+    width: '100%',                
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+  const handleVideo = (id)=>{
+    console.log(id)
+    axios.get(`/movie/${id}/videos?api_key=${apiKey}&language=en-US`).then((response)=>{
+      console.log(response.data);
+      if(response.data.results.length !== 0){
+      setUrl(response.data.results[0])
+      }else{
+        console.log('array empty');
+      }
+    })
+  }
   return (
     <div className='row'>
       <h1 className="title">{props.title}</h1>
       <div className="cards">
         {row.map((obj)=>
-        <img className={props.isSmall?'small-card-image':'card-image'} src={`${imgUrl+obj.backdrop_path}`} alt="" />
+        <img onClick={()=>handleVideo(obj.id)} className={props.isSmall?'small-card-image':'card-image'} src={`${imgUrl+obj.backdrop_path}`} alt="" />
         )}
       </div>
-    </div>
+      <div className='col-md-12' style={{margin:40}}>
+      {url?<YouTube videoId={url.key} opts={opts} />:""}
+      </div>    
+      </div>
   )
 }
 
